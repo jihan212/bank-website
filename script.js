3bb102c77bd3cb7a21fea1348b6e940934bbb26a
snapper <zihanjashim2121@gmail.comm>
2021-01-27 00:43:21 +0600

    Dynamic bank site

--- script.js
+++ script.js
@@ -9,13 +9,40 @@ const loginBtn = document.getElementById("login-btn");
         })
 
 // Deposit button event handler
-const depositBtn = document.getElementById("deposit");
+        const depositBtn = document.getElementById("deposit");
         depositBtn.addEventListener("click", function(){
-            const depositAmount = document.getElementById("depositAmount").value;
-            const depositNumber = parseFloat(depositAmount);
-            const currentDeposit = document.getElementById("currentDeposit").innerText;
-            const currentDepositNumber = parseFloat(currentDeposit);
-            totalDeposit = depositNumber + currentDepositNumber;
-            document.getElementById("currentDeposit").innerText = totalDeposit;
+
+            const depositNumber = getInput("depositAmount");
+
+            spanTextUpdate("currentDeposit", depositNumber);
+            spanTextUpdate("currentBalance", depositNumber);
+
             document.getElementById("depositAmount").value = "";
         })
+// Withdraw button event handler 
+        const withdrawBtn = document.getElementById("withdraw");
+        withdrawBtn.addEventListener("click", function(){
+            
+            const withdrawNumber = getInput("withdrawAmount");
+
+            spanTextUpdate("currentWithdraw", withdrawNumber);
+            spanTextUpdate("currentBalance", -1*withdrawNumber);   
+
+            document.getElementById("withdrawAmount").value = "";
+        })
+
+// function for get input
+        function getInput (id){
+            const amount = document.getElementById(id).value;
+            const amountNumber = parseFloat(amount);
+            return amountNumber
+        }
+
+
+// function for deposit
+        function spanTextUpdate (id , depositNumber){
+            const current = document.getElementById(id).innerText;
+            const currentNumber = parseFloat(current);
+            totalDeposit = depositNumber + currentNumber;
+            document.getElementById(id).innerText = totalDeposit;
+        }
